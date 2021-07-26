ee4ec39a5fadd1647a40033803b4a8f15d64dea6
Alicia Key <Alicia.Key@nrel.gov>
2021-07-26 11:12:55 -0600

    Update __main__.py
    First pass at making a nested dictionary from a row.

--- cpdupload/__main__.py
+++ cpdupload/__main__.py
@@ -3,8 +3,10 @@ This is a basic implementation of a command prompt interface to upload CPD
 data.
 """
 
+from typing import Dict, Any
 from cpdupload.csvingest import CsvIngest, CsvIngestException
 import argparse
+import json
 
 
 def parse_arguments() -> argparse.Namespace:
@@ -14,13 +16,32 @@ def parse_arguments() -> argparse.Namespace:
     return args
 
 
+def store_key(d: Dict, key_path: list, value: Any):
+    head = key_path[0]
+    tail = key_path[1:]
+
+    if len(tail) > 0 and head not in d:
+        d[head] = {}
+        store_key(d[head], tail, value)
+    elif len(tail) > 0 and head in d:
+        store_key(d[head], tail, value)
+    else:
+        d[head] = value
+
+
 def main() -> None:
     args = parse_arguments()
     print(f"Attempting to parse {args.csv}")
     try:
         ingest = CsvIngest(args.csv)
         rows = ingest.load_csv()
-        print(rows)
+        for row in rows:
+            top_dict = {}
+            for key, value in row.items():
+                key_path = key.split(".")
+                store_key(top_dict, key_path, value)
+            top_json = json.dumps(top_dict)
+            print(top_json)
     except CsvIngestException as err:
         print(err)
 
