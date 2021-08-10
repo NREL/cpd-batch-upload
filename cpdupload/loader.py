9a55db05717c4869303978df09d2e543d6bd99fe
Alicia Key <Alicia.Key@nrel.gov>
2021-08-10 14:01:25 -0600

    Update loader.py
    Do not need a second health check.

--- cpdupload/loader.py
+++ cpdupload/loader.py
@@ -60,9 +60,7 @@ class Loader:
         """
         try:
             api = API(self.api)
-            api.health_check()
-            print("API health check successful...")
-
+            
             if self.input_filename.endswith(".csv"):
                 ingest = CsvIngest(self.input_filename)
                 rows: List[Dict[str, Union[int, str, float]]] = ingest.load_csv()
