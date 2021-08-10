a1400adf7fef7bc56bbc16e9a5b3e387b6f6e2c1
Alicia Key <Alicia.Key@nrel.gov>
2021-08-10 14:30:49 -0600

    Update __main__.py
    Create comments in the main module to explain what each group of lines do.

--- cpdupload/__main__.py
+++ cpdupload/__main__.py
@@ -8,12 +8,21 @@ from cpdupload.loader import Loader, LoaderException
 
 
 try:
+    # Parse command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", help="file to import, .csv or .json format")
     parser.add_argument("--api", help="URL to API.")
     args = parser.parse_args()
+
+    # Create a loader which wraps file reading, parsing, and API connection functionality
     loader = Loader(input_filename=args.input, api=args.api)
+
+    # Send the parsed file to the API
     loader.send_adsorption_measurement_to_api()
+
+    # Report a success message to the user
     print("Successfully loaded adsorption measurement(s) to the API.")
+
+# Print any errors that happen in a user-friendly format
 except LoaderException as e:
     print(e)
