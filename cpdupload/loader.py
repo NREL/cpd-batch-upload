cac8a7a5bf6b7e1e832a0a69ff732872e56dad49
Alicia Key <Alicia.Key@nrel.gov>
2021-08-10 13:32:50 -0600

    Update loader.py
    Check API and filename at instantiation.

--- cpdupload/loader.py
+++ cpdupload/loader.py
@@ -1,12 +1,42 @@
+import os
+from api import API, APIException
+
+
 class Loader:
     """
     The Loader class unifies the API load workflow. It has a method to accept a filename
     and attempt to upload it to the API.
     """
 
-    def __init__(self, input_filename: str):
-        
+    def __init__(self, input_filename: str, api: str):
+        """
+        This constructor accepts an input filename as a parameter and checks to
+        ensure the filename exists before any load operation happens.
+
+        Parameters
+        ----------
+        input_filename: str
+            The input filename for the load operation.
+
+        api: str
+            The base URL of the CPD API server.
+
+        Raises
+        ------
+        LoaderException
+            Raises a LoaderException if the filename is not found.
+        """
+        if not os.path.isfile(input_filename):
+            raise LoaderException(f"Cannot import {input_filename} because the file was not found.")
+
         self.input_filename = input_filename
+
+        try:
+            api_check = API(api)
+            api_check.health_check()
+            self.api = api
+        except APIException as e:
+            raise LoaderException(e)
 
 
 class LoaderException(Exception):
@@ -28,7 +58,50 @@ class LoaderException(Exception):
 
     Additionally, a LoaderException can be raised on its own by the Loader class
     for errors that fall outside the scope of the other three exceptions.
+
+    For the message in the exception, emphasis should be placed on an error message
+    that can be shown to a user with some actionable meaning.
     """
 
     def __init__(self, message: str):
+        """
+        Accepts a message that should be descriptive for display to a user.
+
+        Parameters
+        ----------
+        message: str
+            An error message useful to an end user.
+        """
         super(LoaderException, self).__init__(message)
+
+
+# def main() -> None:
+#     args = parse_arguments()
+#     try:
+#         api = API(args.api)
+#         api.health_check()
+#         print("API health check successful...")
+#
+#         if args.input.endswith(".csv"):
+#             ingest = CsvIngest(args.input)
+#             print("CSV parse successful...")
+#             rows: List[Dict[str, Union[int, str, float]]] = ingest.load_csv()
+#             builder = JSONBuilder()
+#             json_for_upload = builder.parse_rows(rows)
+#             print("JSON build successful...")
+#             with open("output.json", "w") as file:
+#                 file.write(json.dumps(json_for_upload))
+#         else:
+#             with open(args.input, "r") as file:
+#                 json_for_upload = json.loads(file.read())
+#             print("JSON file read successfully...")
+#
+#         api.adsorption_measurement_load(json_for_upload)
+#         print("Upload to API successful!")
+#
+#     except CsvIngestException as err:
+#         print(f"Error while parsing csv file: {err}")
+#     except JSONBuilderException as err:
+#         print(f"Error while building the JSON: {err}")
+#     except APIException as err:
+#         print(f"Error connecting to the CKAN API: {err}")
