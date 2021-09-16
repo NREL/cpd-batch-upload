9b0c447a6d59e84fb19f9045d2278f58740f4186
Alicia Key <Alicia.Key@nrel.gov>
2021-09-16 10:46:19 -0600

    Update authentication.py
    Support for basic username password authentication. This will need to be changed to SRP authentication before release.

--- cpdupload/authentication.py
+++ cpdupload/authentication.py
@@ -1,31 +1,37 @@
+import logging
+import getpass
 import boto3
-import os
 
 
 class Authentication:
-    def __init__(self, username, password):
+    def __init__(self, client_id, username):
+        self.logger = logging.getLogger(__name__)
+        self.client_id = client_id
         self.username = username
-        self.password = password
-        self.client_id = os.environ.get("COGNITO_CLIENT_ID", "")
+        self.password = None
+
+    def prompt_password(self):
+        self.password = getpass.getpass()
 
     def authenticate_and_get_token(self):
         client = boto3.client('cognito-idp')
 
-        resp = client.initiate_auth(
-            ClientId=self.client_id,
-            AuthFlow="USER_PASSWORD_AUTH",
-            AuthParameters={
-                "USERNAME": self.username,
-                "PASSWORD": self.password
-            }
-        )
+        try:
+            resp = client.initiate_auth(
+                ClientId=self.client_id,
+                AuthFlow="USER_PASSWORD_AUTH",
+                AuthParameters={
+                    "USERNAME": self.username,
+                    "PASSWORD": self.password
+                }
+            )
+        except Exception as e:
+            raise AuthenticationException(f"User authentication failed. {e}")
 
+        self.logger.info(f"Authentication for user {self.username} was successful.")
         return resp['AuthenticationResult']['AccessToken']
 
 
-if __name__ == "__main__":
-    auth = Authentication(
-        os.environ.get("COGNITO_USERNAME", "no username provided"),
-        os.environ.get("COGNITO_PASSWORD", "no password provided")
-    )
-    print(auth.authenticate_and_get_token())
+class AuthenticationException(Exception):
+    def __init__(self, message):
+        super(AuthenticationException, self).__init__(message)
