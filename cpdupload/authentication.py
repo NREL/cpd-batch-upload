7533309701bee249b996975f03d71608c1f0c950
Alicia Key <Alicia.Key@nrel.gov>
2021-09-17 11:24:09 -0600

    Update authentication.py
    Create authentication test code.

--- cpdupload/authentication.py
+++ cpdupload/authentication.py
@@ -1,19 +1,22 @@
 import logging
 import getpass
+import os
+
 import boto3
 
 
 class Authentication:
-    def __init__(self, client_id, username):
+    def __init__(self, client_id: str, pool_id: str, username: str):
         self.logger = logging.getLogger(__name__)
         self.client_id = client_id
+        self.pool_id = pool_id
         self.username = username
         self.password = None
 
     def prompt_password(self):
         self.password = getpass.getpass()
 
-    def authenticate_and_get_token(self):
+    def authenticate_and_get_token(self) -> str:
         client = boto3.client('cognito-idp')
 
         try:
@@ -35,3 +38,13 @@ class Authentication:
 class AuthenticationException(Exception):
     def __init__(self, message):
         super(AuthenticationException, self).__init__(message)
+
+
+if __name__ == "__main__":
+    client_id = os.environ.get("COGNITO_CLIENT_ID", "")
+    pool_id = os.environ.get("COGNITO_POOL_ID", "")
+    username = os.environ.get("COGNITO_USERNAME", "")
+    auth = Authentication(client_id, pool_id, username)
+    auth.prompt_password()
+    token = auth.authenticate_and_get_token()
+    print(token)
