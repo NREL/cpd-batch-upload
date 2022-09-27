c1fd23bc548ed6509f836abd7e488ae1f840817a
ChemE-SeanT <90356239+ChemE-SeanT@users.noreply.github.com>
2022-09-27 14:00:40 -0600

    Add files via upload

---
+++ integration_examples/VASP_calculateBE/Ag100/Clean/4x4/submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#SBATCH --nodes=1                   # Number of nodes
+#SBATCH --ntasks-per-node=36        # This is a number we should do some benchmarking for
+#SBATCH --time=04:00:00            
+#SBATCH --account=ccpc             # Allocation, use ccpc or electrochem
+
+module purge
+module load mkl/2020.1.217
+module load intel-mpi
+module load vasp/5.4.4_centos77
+vasp='vasp_std'              # use ‘vasp_gam’ for gamma point calculations
+
+date > a
+srun $vasp>out
+date > b
